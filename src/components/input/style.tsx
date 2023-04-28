8d3bd716784daa5a382865e8802a3250306e55d3
GustavoPec <gustavo@softilux.com.br>
2023-04-27 22:31:21 -0300

    Layout login atualizado

---
+++ src/components/input/style.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  
+  input{
+    width: 15rem;
+    height: 2rem;
+    border-radius: 0.2rem;
+    padding: 0.5rem;
+    border-style: none;
+  }
+`;
--- src/pages/Login/Login.module.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import Button from '@material-ui/core/Button';
-
-const ButtonLogin = styled(Button)`
-marginTop: theme.spacing(3),
-`;
-
-export default ButtonLogin;
